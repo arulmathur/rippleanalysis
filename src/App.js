76dbde1cd5803bca5330e5befd4208a8b57f29a6
Arul Mathur <arulmathur1@gmail.com>
2023-03-07 19:37:46 -0800

    Displays count of number of each item sold

--- src/App.js
+++ src/App.js
@@ -7,21 +7,50 @@ function App() {
   const [data, setData] = useState([]);
   const [columnArray, setColumn] = useState([]);
   const [values, setValues] = useState([]);
+  const [bottleCount, setBottleCount] = useState(0);
+  const [stickerCount, setStickerCount] = useState(0);
+  const [crewneckCount, setCrewneckCount] = useState(0);
+  const [hoodieCount, setHoodieCount] = useState(0);
+  const [tumblerCount, setTumblerCount] = useState(0);
+  const [tshirtCount, setTshirtCount] = useState(0);
 
   const handleFile = (event) => {
     Papa.parse(event.target.files[0], {
       header: true,
-      skipEmptyLines: true,
+      skipEmptyLines: false,
       complete: function (result) {
         const columnArray = ["Email", "Subtotal", "Lineitem name", "Lineitem sku"];
-        const valuesArray = result.data.map((d) => [d.Email, d.Subtotal, d["Lineitem name"], d["Lineitem sku"]
-        ]);
+        const valuesArray = result.data.map((d) => {
+          const row = [d.Email, d.Subtotal, d["Lineitem name"], d["Lineitem sku"]];
+          Object.keys(d).forEach((col) => {
+            if (col.includes("Lineitem name") && d[col].includes("Bottle")) {
+              setBottleCount((prevCount) => prevCount + 1);
+            }
+            if (col.includes("Lineitem name") && d[col].includes("Sticker")) {
+              setStickerCount((prevCount) => prevCount + 1);
+            }
+            if (col.includes("Lineitem name") && d[col].includes("Crewneck")) {
+              setCrewneckCount((prevCount) => prevCount + 1);
+            }
+            if (col.includes("Lineitem name") && d[col].includes("Hoodie")) {
+              setHoodieCount((prevCount) => prevCount + 1);
+            }
+            if (col.includes("Lineitem name") && d[col].includes("Tumbler")) {
+              setTumblerCount((prevCount) => prevCount + 1);
+            }
+            if (col.includes("Lineitem name") && d[col].includes("T-Shirt")) {
+              setTshirtCount((prevCount) => prevCount + 1);
+            }
+          });
+          return row;
+        });
+
         setData(result.data);
         setColumn(columnArray);
         setValues(valuesArray);
       }
-    })
-  }
+    });
+  };
 
 
   return (
@@ -30,6 +59,27 @@ function App() {
       </input>
 
       <br />
+
+      <p>
+        Bottles sold: {bottleCount}
+      </p>
+      <p>
+        Stickers sold: {stickerCount}
+      </p>
+      <p>
+        Crewnecks sold: {crewneckCount}
+      </p>
+      <p>
+        Hoodies sold: {hoodieCount}
+      </p>
+      <p>
+        Tumblers sold: {tumblerCount}
+      </p>
+      <p>
+        T-Shirts sold: {tshirtCount}
+      </p>
+
+      <hr></hr>
 
       <table style={{ borderCollapse: "collapse", border: "1px solid black", margin: "5px auto" }}>
         <thead>
@@ -45,12 +95,13 @@ function App() {
               {v.map((value, i) => (
                 <td key={i}>{value}</td>
               ))}
-
             </tr>
           ))}
         </tbody>
 
       </table>
+
+
 
     </div>
   );
